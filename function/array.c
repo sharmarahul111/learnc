0a9b141cf3ce6fb325cb41d9bb8fa9b00b108343
Sharmarahul111 <sharmarahul111.psr@gmail.com>
2026-01-23 07:38:51 +0545

    Function and sort string

---
+++ function/array.c
@@ -0,0 +1,58 @@
+void input(int x[], int n);
+void display(int x[], int n);
+int maximum(int x[], int n);
+int minimum(int x[], int n);
+int sum(int x[], int n);
+int i;
+
+#include<stdio.h>
+int main(){
+  // code
+  int x[50];
+  int n;
+  printf("Enter size of number:");
+  scanf("%d", &n);
+  input(x,n);
+  display(x,n);
+  printf("\nMin=%d", minimum(x,n));
+  printf("\nMax=%d", maximum(x,n));
+  printf("\nSum=%d\n", sum(x, n ));
+  return 0;
+}
+void input(int x[], int n){
+  printf("Enter values of array elements:");
+  for (i=0;i<n;i++) {
+    scanf("%d", &x[i]);
+  }
+}
+void display(int x[], int n){
+  printf("\nThe array:");
+  for (i=0;i<n;i++) {
+    printf("%d\t", x[i]);
+  }
+}
+int maximum(int x[], int n){
+  int max = x[0];
+  for (i=0;i<n;i++) {
+    if (x[i]<max) {
+      max=x[i];
+    }
+  }
+  return max;
+}
+int minimum(int x[], int n){
+  int min = x[0];
+  for (i=0;i<n;i++) {
+    if (x[i]>min) {
+      min=x[i];
+    }
+  }
+  return min;
+}
+int sum(int x[], int n){
+  int sum=0;
+  for (i=0;i<n;i++) {
+    sum+=x[i];
+  }
+  return sum;
+}
---
+++ function/string-sort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+void input(char x[][20], int n);
+void display(char x[][20], int n);
+void sort(char x[][20], int n);
+int i,j;
+
+
+int main(){
+  int x[50][20];
+  int n;
+  printf("Enter size of strings:");
+  scanf("%d", &n);
+  input(x,n);
+  sort(x,n);
+  printf("The sorted array:");
+  display(x,n);
+  return 0;
+}
+void sort(char x[][20], int n){
+    char temp[20];
+    for(i=0;i<n-1;i++){
+        for(j=i;j<n;j++){
+            if(strcmp(x[i], x[j])>0){
+                strcpy(temp, x[i]);
+                strcpy(x[i], x[j]);
+                strcpy(x[j], temp);
+            }
+        }
+    }
+}
+void input(char x[][20], int n){
+  printf("Enter strings elements:");
+  for (i=0;i<n;i++) {
+    scanf("%s", x[i]);
+  }
+}
+void display(char x[][20], int n){
+  for (i=0;i<n;i++) {
+    printf("%s\t", x[i]);
+  }
+  printf("\n");
+}
+
