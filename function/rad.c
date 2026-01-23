19d1b3569b72783e38bd1c3ebb731188747b512b
Sharmarahul111 <sharmarahul111.psr@gmail.com>
2026-01-23 07:51:09 +0545

    read, add, display

---
+++ function/rad.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int read();
+void display(int);
+int sum(int x, int y);
+int i;
+
+int main(){
+  int x,y;
+  x=read();
+  y=read();
+  display(sum(x,y));
+  return 0;
+}
+int read(){
+  int x;
+  printf("Enter a number:");
+  scanf("%d", &x);
+  return x;
+
+}
+void display(int x){
+  printf("\nThe number is: %d\n",x);
+}
+int sum(int x, int y){
+  return x+y;
+}
